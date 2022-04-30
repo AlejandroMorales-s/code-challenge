988b3eb8d0ecd384a1af4233472e1953e7be97ab
Alejandro <alejandrom.am33@gmail.com>
2022-04-30 18:07:07 -0500

    add: nueva clase agregada al controlador y su test

--- test/controllers/studentController.test.js
+++ test/controllers/studentController.test.js
@@ -9,4 +9,11 @@ describe('StudentController', () => {
         const mails = StudentController.getEmails();
         expect(mails[0]).toBe("Todd@visualpartnership.xyz");
     });
+    test('Students with more than 500 credits', () => {
+        const partner = StudentController.getStudentsWithMoreThan500Credits();
+        let status;
+        partner.forEach((element) => element.credits > 500 ? status = true : status = false)
+        
+        expect(status).toBe(true);
+    });
 })
