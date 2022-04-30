785e25b10245308683b2e627bc3abfa36ae509be
Alejandro <alejandrom.am33@gmail.com>
2022-04-30 18:11:32 -0500

    fix: agregando eslint

--- test/controllers/studentController.test.js
+++ test/controllers/studentController.test.js
@@ -1,19 +1,19 @@
-const StudentController = require('./../../lib/controllers/studentController');
+const StudentController = require("./../../lib/controllers/studentController");
 
-describe('StudentController', () => {
-    test('Get all students and their fields', () => {
+describe("StudentController", () => {
+    test("Get all students and their fields", () => {
         const partner = StudentController.getAllStudentsAndTheirFields();
         expect(partner).toBeDefined();
     });
-    test('Get all students and their fields', () => {
+    test("Get all students and their fields", () => {
         const mails = StudentController.getEmails();
         expect(mails[0]).toBe("Todd@visualpartnership.xyz");
     });
-    test('Students with more than 500 credits', () => {
+    test("Students with more than 500 credits", () => {
         const partner = StudentController.getStudentsWithMoreThan500Credits();
         let status;
-        partner.forEach((element) => element.credits > 500 ? status = true : status = false)
+        partner.forEach((element) => element.credits > 500 ? status = true : status = false);
         
         expect(status).toBe(true);
     });
-})
+});
--- test/services/studentService.test.js
+++ test/services/studentService.test.js
@@ -1,26 +1,26 @@
-const StudentService = require('./../../lib/services/studentService');
-const Reader = require('./../../lib/utils/reader');
+const StudentService = require("./../../lib/services/studentService");
+const Reader = require("./../../lib/utils/reader");
 
-describe('StudentService', () => {
-    test('getAllStudentsAndTheirFields', () => {
+describe("StudentService", () => {
+    test("getAllStudentsAndTheirFields", () => {
         const partners = Reader.readJsonFile("visualpartners.json");
         const partner = StudentService.getAllStudentsAndTheirFields(partners);
 
         expect(partner).toBeDefined();
     });
 
-    test('Students email', () => {
+    test("Students email", () => {
         const partners = Reader.readJsonFile("visualpartners.json");
         const partner = StudentService.getEmails(partners);
         
         expect(partner[0]).toBe("Todd@visualpartnership.xyz");
     });
 
-    test('Students with more than 500 credits', () => {
+    test("Students with more than 500 credits", () => {
         const partners = Reader.readJsonFile("visualpartners.json");
         const partner = StudentService.getStudentsWithMoreThan500Credits(partners);
         let status;
-        partner.forEach((element) => element.credits > 500 ? status = true : status = false)
+        partner.forEach((element) => element.credits > 500 ? status = true : status = false);
         
         expect(status).toBe(true);
     });
