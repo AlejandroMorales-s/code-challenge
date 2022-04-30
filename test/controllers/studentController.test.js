db9bdd670e99704f5234683723c0c8bbe5901bfe
Alejandro <alejandrom.am33@gmail.com>
2022-04-30 16:09:42 -0500

    add: clase para obtener emails al controlador y su test

--- test/controllers/studentController.test.js
+++ test/controllers/studentController.test.js
@@ -3,7 +3,10 @@ const StudentController = require('./../../lib/controllers/studentController');
 describe('StudentController', () => {
     test('Get all students and their fields', () => {
         const partner = StudentController.getAllStudentsAndTheirFields();
-
         expect(partner).toBeDefined();
+    });
+    test('Get all students and their fields', () => {
+        const mails = StudentController.getEmails();
+        expect(mails[0]).toBe("Todd@visualpartnership.xyz");
     });
 })
